56fd29ecc089f7c859eb920d691e5080294b9268
Puiching-Memory <1138663075@qq.com>
2025-07-08 12:14:32 +0000

    feat: 添加 zarr 格式数据转换脚本
    - 新增 conver2zarr.py 脚本，用于将 Hugging Face 数据集转换为 zarr 格式
    - 修改 conver2image.py 脚本，使用多进程处理图像并优化输出格式
    - 更新 README.md，添加 zarr 库的安装说明

--- lib/tools/conver2image.py
+++ lib/tools/conver2image.py
@@ -2,10 +2,11 @@
 # 171GB -> train:199GB + 
 from datasets import load_dataset
 import os
-import uuid
+import hashlib
 from concurrent.futures import ThreadPoolExecutor, as_completed, ProcessPoolExecutor
+from tqdm import tqdm
 
-output_dir = "./.cache/imagenet-1k-avif-256"
+output_dir = "./.cache/imagenet-1k-jpeg-256"
 os.makedirs(output_dir, exist_ok=True)
 os.makedirs(os.path.join(output_dir,"train"), exist_ok=True)
 os.makedirs(os.path.join(output_dir,"validation"), exist_ok=True)
@@ -18,20 +19,26 @@ dataset = load_dataset("./lib/dataset/imagenet_1k_datasets.py",
                     )
 dataset.with_format("torch")
 
-def process_item(d, split):
+progress = tqdm(total=len(dataset["train"])+len(dataset["validation"])+len(dataset["test"]), desc="Converting Progress",leave=True)
+
+def process_item(index, d, split):
     image = d["image"]
     label = d["label"]
+    if image.mode != 'RGB': image = image.convert('RGB')
     image = image.resize((256, 256))
-    UID = uuid.uuid4()
-    file_name = f"{str(label)}-{str(UID)}.avif"
-    print(os.path.join(output_dir, split ,file_name))
-    image.save(os.path.join(output_dir, split ,file_name), format="AVIF", lossless=True, quality=100, speed=0)
-    return file_name
+    hash_object = hashlib.sha256(image.tobytes())
+    file_name = f"{label}-{hash_object.hexdigest()}.jpeg"
+    if os.path.exists(os.path.join(output_dir, split ,file_name)):return
+    image.save(os.path.join(output_dir, split ,file_name), format="JPEG", quality=95, optimize=True)
+
+    progress.n = index
+    progress.set_description(f"Converting {os.path.join(output_dir, split ,file_name)}")
+    progress.update()
 
 for split in dataset.keys():  # 遍历所有切片
     os.makedirs(os.path.join(output_dir, split), exist_ok=True)  # 为每个切片创建目录
     with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
-        futures = [executor.submit(process_item, d, split) for d in dataset[split]]
+        futures = [executor.submit(process_item, index, d, split) for index,d in enumerate(dataset[split])]
         
         for future in as_completed(futures):
-            file_name = future.result()
+            future.result()
